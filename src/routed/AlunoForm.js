d995c8fb33ab084c6bc8b1b1752a8bd43e679842
Fernando <fernandoasfilho74@gmail.com>
2022-05-30 10:30:04 -0300

    30/05/2022

--- src/routed/AlunoForm.js
+++ src/routed/AlunoForm.js
@@ -8,8 +8,11 @@ import InputMask from 'react-input-mask'
 import MenuItem from '@mui/material/MenuItem'
 import Toolbar from '@mui/material/Toolbar'
 import Button from '@mui/material/Button'
-import alertBar from '../ui/alertBar'
-
+import AlertBar from '../ui/alertBar'
+import ModalProgress from '../ui/ModalProgress'
+import api from '../api'
+import { useNavigate, useParams } from 'react-router-dom'
+import ConfirmDialog from '../ui/ConfirmDialog'
 
 const useStyles = makeStyles(theme => ({
     form: {
@@ -24,10 +27,11 @@ const useStyles = makeStyles(theme => ({
             marginBottom: '24px'
         }
     },
-    Toolbar:{
-        width:'100%',
+    toolbar: {
+        width: '100%',
         justifyContent: 'space-around'
     }
+
 }))
 
 const unidadesFed = [
@@ -42,69 +46,197 @@ const unidadesFed = [
 ]
 
 const turmas = [
-    { sigla: 'ING10', descricao: '[ING10] Inglês iniciante'},
-    { sigla: 'ESP10', descricao: '[ESP10] Espanhol iniciante'},
-    { sigla: 'FRA10', descricao: '[FRA10] Francês iniciante'}
+    { sigla: 'ESP10', descricao: '[ESP10] Espanhol iniciante' },
+    { sigla: 'FRA10', descricao: '[FRA10] Francês iniciante' },
+    { sigla: 'ING10', descricao: '[ING10] Inglês iniciante' }
 ]
 
 export default function AlunoForm() {
 
     const classes = useStyles()
 
+    const navigate = useNavigate()
+
+    const params = useParams()//consegue Verifica se na rota tem parametros
+
     const [state, setState] = React.useState(
         // Lazy initalizer
         () => ({
-            //campos correspondentes a controles de seleção
-            //precisam ter um valor inicial
-            aluno: {uf: '', turma: ''},
-            alertSeverity:'success',
+            // Campos correspondentes a controles de seleção
+            // precisam ter um valor inicial  
+            aluno: { 
+            nome:'',    
+            uf: '', 
+            turma: '', 
+            data_nascimento: '',
+            doc_identidade:'',
+            cpf:'',
+            logradouro:'',
+            num_imovel:'',
+            complemento:'',
+            bairro:'',
+            municipio:'',
+            uf:'',
+            telefone:'',
+            email:'',
+            turma:''},
+            alertSeverity: 'success',
             isAlertOpen: false,
-            alertMessage: ''
+            alertMessage: '',
+            isModalProgressOpen: false,
+            pageTitle:'Cadastrar novo aluno',
+            isDialogOpen: false
         })
     )
     const {
         aluno,
         alertSeverity,
         isAlertOpen,
-        alertMessage
-        } = state 
+        alertMessage,
+        isModalProgressOpen,
+        pageTitle,
+        isDialogOpen
+    } = state
 
-        function handleInputChange(event, fieldName = event.target.id) {
-            console.log(`fieldName: ${fieldName}, value: ${event?.target?.value}`)//? evita erros de carregamento
+    React.useEffect(() => {
 
-        
-        //Sincroniza o valor do input com a variavel de estado
+        //Se houver parameto na rota, estamos editando um registro já existente
+        //portanto precisamos buscar os dados desse registro
+        //para carregar nos campos e editar
+        if(params.id){
+            fetchData()
+        }
+
+    }, []) //Dependencias vazias, executa só no carregamento do componente
+
+    async function fetchData(){
+        try{
+            const response = await api.get(`alunos/${params.id}`)
+            setState({
+                ...state,
+                aluno: response.data,
+                pageTitle:'Editando aluno id. ' + params.id
+            })
+        }
+        catch(erro){
+            setState({
+                ...state,
+                alertSeverity: 'error',
+                alertMessage: 'ERRO' + erro.message,
+                isAlertOpen: true,
+                pageTitle:'## ERRO ##'
+            })
+        }
+    }
+
+    function handleInputChange(event, fieldName = event.target.id) {
+        console.log(`fieldName: ${fieldName}, value: ${event?.target?.value}`)
+
+        // Sincroniza o valor do input com a variável de estado
         const newAluno = {...aluno}     // Tira uma cópia do aluno
-        // tira uma cópia do aluno
-        
-        // o componente desktopdatepicker envia newvalue em vez de
-        // event: portando, é necessário tratamento especifico para ele
+
+        // O componente DesktopDatePicker envia newValue em vez de
+        // event; portanto, é necessário tratamento específico para ele
         if(fieldName === 'data_nascimento') newAluno[fieldName] = event
         else newAluno[fieldName] = event.target.value // Atualiza o campo
         
-        newAluno[fieldName] = event.target.value //atualiza o campo
-        setState({...state, aluno: newAluno})
+        setState({ ...state, aluno: newAluno })
     }
+
     function handleAlertClose(event, reason) {
         if (reason === 'clickaway') {
           return;
         }
     
-        // Fecha a barra de alerta
-        setState({...state, isAlertOpen: false})
-      }
+        // Fecha a barra de alerta e o progresso modal
+        setState({...state, isAlertOpen: false, isModalProgressOpen: false})
+
+        // Se os dados forem salvos com sucesso, volta para a página
+        // de listagem
+        if(alertSeverity === 'success') navigate('/aluno')
+    }
+
+    function handleFormSubmit(event) {
+        event.preventDefault()  // Evita o recarregamento da página
+        saveData()
+    }
+
+    async function saveData() {
+        // Exibe o progresso modal
+        setState({...state, isModalProgressOpen: true})
+
+        try {
+            //Se params.id existe entao estamos editando, verno put
+            if(aluno.id) await api.put(`alunos/${params.id}`, aluno)
+            //senao, estamos criando um novo
+            else await api.post(`alunos`, aluno)
+            await api.post('alunos', aluno)
+            setState({
+                ...state,
+                isAlertOpen: true,
+                alertSeverity: 'success',
+                alertMessage: 'Dados salvos com sucesso',
+                //isModalProgressOpen: true
+            })
+        } 
+        catch(erro) {
+            setState({
+                ...state,
+                isAlertOpen: true,
+                alertSeverity: 'error',
+                alertMessage: 'ERRO: ' + erro.message,
+                //isModalProgressOpen: true
+            })
+        }
+    }
+
+    function isFormModified(){
+        for(let field in aluno){
+            if(aluno[field] !== '') return true
+        }
+        return false
+    }
+
+    function handleVoltarButtonClick(){
+        if(isFormModified()){
+            //se o formulario tiver sido editado, chama a caixa de dialogo
+            //para perguntar ao usuario se realmente quer voltar, perdendo dados
+            setState({...state, isDialogOpen: true}) 
+        }
+        //Se nao houve modificação retorna diretamene
+        else navigate('/aluno')
+    }
+    function handleDialogClose(answer){
+        //fecha a caixa de dialogo
+        setState({...state, isDialogOpen: false})
+
+        //se o usuário tiver res´pmdodp "sim", volta à listagem
+        if(answer) navigate('/aluno')
+    }
+
     return (
         <>
-            <alertBar 
-            severity={alertSeverity} 
-            open={isAlertOpen}
-            onClose={handleAlertClose}
-          >
-            {alertMessage}
-          </alertBar>
-            <h1>Cadastro de alunos</h1>
+            <AlertBar 
+                severity={alertSeverity} 
+                open={isAlertOpen}
+                onClose={handleAlertClose}
+            >
+                {alertMessage}
+            </AlertBar>
 
-            <form className={classes.form}>
+            <ModalProgress open={isModalProgressOpen} />
+
+            <ConfirmDialog 
+            title="Os dados foram modificados" 
+            open={isDialogOpen}
+            onClose={handleDialogClose}
+          >
+            Deseja realmente Descartar as informações não salvas?
+          </ConfirmDialog>
+            
+            <h1>{pageTitle}</h1>
+
+            <form className={classes.form} onSubmit={handleFormSubmit}>
                 
                 <TextField 
                     id="nome" 
@@ -148,7 +280,6 @@ export default function AlunoForm() {
                     mask="999.999.999-99"
                     value={aluno.cpf}
                     onChange={event => handleInputChange(event, 'cpf')}
-
                 >
                     {
                         () => <TextField 
@@ -195,6 +326,17 @@ export default function AlunoForm() {
                 />
 
                 <TextField 
+                    id="bairro" 
+                    label="Bairro"
+                    value={aluno.bairro}
+                    variant="filled"
+                    placeholder="Informe o bairro"
+                    required
+                    fullWidth
+                    onChange={handleInputChange}
+                />
+
+                <TextField 
                     id="municipio" 
                     label="Município"
                     value={aluno.municipio}
@@ -213,7 +355,7 @@ export default function AlunoForm() {
                     placeholder="Informe a UF"
                     required
                     fullWidth
-                    select  
+                    select
                     onChange={event => handleInputChange(event, 'uf')}
                 >
                     {
@@ -224,8 +366,9 @@ export default function AlunoForm() {
                         ))
                     }
                 </TextField>
+
                 <InputMask
-                    mask="(99) 99999.9999"
+                    mask="(99) 99999-9999"
                     value={aluno.telefone}
                     onChange={event => handleInputChange(event, 'telefone')}
                 >
@@ -272,21 +415,24 @@ export default function AlunoForm() {
                     }
                 </TextField>
 
-                <Toolbar className={classes.Toolbar}>
+                <Toolbar className={classes.toolbar}>
                     <Button
-                    variant="Contained"
-                    color="secondary"
-                    type="submit"
+                        variant="contained"
+                        color="secondary"
+                        type="submit"
                     >
                         Enviar
                     </Button>
                     <Button
                         variant="outlined"
+                        onClick={handleVoltarButtonClick}
                     >
                         Voltar
                     </Button>
                 </Toolbar>
+
             </form>
+
             <p>{JSON.stringify(aluno)}</p>
         </>
     )
