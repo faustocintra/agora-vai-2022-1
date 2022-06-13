5713879e1ce7dfb8656bbc9fa2c4a2c1c6a031f4
Fernando <fernandoasfilho74@gmail.com>
2022-06-13 09:04:26 -0300

    finalizado

---
+++ src/prova2/App.js
@@ -0,0 +1,81 @@
+import './App.css';
+import AppHeader from './ui/AppHeader'
+import AppFooter from './ui/AppFooter'
+import { createTheme, ThemeProvider } from '@mui/material'
+import { blue, amber } from '@mui/material/colors'
+import Box from '@mui/material/Box'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+import AlunoList from './routed/AlunoList'
+import AlunoForm from './routed/AlunoForm'
+
+import CursosList from './routed/CursosList'
+import CursoForm from './routed/CursoForm'
+
+import ProfessorList from './routed/ProfessorList'
+import ProfessorForm from './routed/ProfessorForm'
+import Sobre from './routed/Sobre'
+import HomePage from './routed/HomePage';
+
+const customTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: blue[600]
+    },
+    secondary: {
+      main: amber['A400']
+    }
+  }
+})
+
+function App() {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <Box sx={{
+        minHeight: '100vh',     // 100% da altura da área de exibição
+        marginBottom: '48px',   // Desconta a altura do AppFooter
+        backgroundColor: customTheme.palette.background.default,
+        color: customTheme.palette.text.primary
+      }}>
+        
+        <BrowserRouter>
+          <AppHeader />
+          <Box component="main" sx={{ margin: '24px' }}>
+            <Routes>
+
+              <Route path="/" element={<HomePage />} />
+      
+              <Route path="/aluno" element={<AlunoList />} />
+              <Route path="/aluno/novo" element={<AlunoForm />} />
+              <Route path="/aluno/:id" element={<AlunoForm />} />
+
+              <Route path="/professor" element={<ProfessorList />} />
+              <Route path="/professor/novo" element={<ProfessorForm />} />
+              <Route path="/professor/:id" element={<ProfessorForm />} />
+
+              <Route path="/cursos" element={<CursosList />} />
+              <Route path="/curso/novo" element={<CursoForm />} />
+              <Route path="/curso/:id" element={<CursoForm />} />
+
+              <Route path="Sobre" element={<Sobre />} />
+            </Routes>
+            
+          </Box>
+          
+
+
+          
+          <AppFooter />
+          
+        </BrowserRouter>
+        
+      </Box>
+      
+
+    </ThemeProvider>
+  );
+}
+
+export default App;
